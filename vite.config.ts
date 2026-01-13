1f868553946a75a5c8f1fed89414654afe139db7
Maghfirah <firahmagh485@gmail.com>
2026-01-13 12:26:31 +0700

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/Digital-Library-Blog/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
